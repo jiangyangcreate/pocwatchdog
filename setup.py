473ad18595f4515433554a86a9ff47823b997e15
jiangyangcreate <jiangyangcreate@gmail.com>
2024-10-11 13:42:34 +0800

    🚀

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pocwatchdog',
-    version='1.1.3',
+    version='1.1.4',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
     python_requires=">=3.6",
-    install_requires=[
-        "schedule",
-    ],
+    install_requires="schedule>=1.1.0",
 )
