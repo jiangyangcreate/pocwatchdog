657b7b9df17f79419f4e4b23bfb0ff4d69c41f6b
jiangyangcreate <jiangyangcreate@gmail.com>
2024-10-11 11:48:33 +0800

    1.1.3

--- build/lib/pocwatchdog/__init__.py
+++
@@ -1,1 +0,0 @@
-from .task_scheduler import run
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pocwatchdog',
-    version='1.1.1',
+    version='1.1.3',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
